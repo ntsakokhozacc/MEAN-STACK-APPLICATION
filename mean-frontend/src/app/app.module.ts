fa85ef14fcce2d833af6f6f5e4eaaba94ab5537d
Ntsako Khoza <ntsako.khoza@dadevs.co.za>
2022-01-20 11:12:27 +0200

    material

--- mean-frontend/src/app/app.module.ts
+++ mean-frontend/src/app/app.module.ts
@@ -3,7 +3,8 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PostCreateComponent } from './posts/post-create/post-create.component'
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { PostCreateComponent } from './posts/post-create/post-create.component'
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

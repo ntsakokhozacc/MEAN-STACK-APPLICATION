5a2ef0a858fe16f3590cf440f3628638ab31396e
Ntsako Khoza <ntsako.khoza@dadevs.co.za>
2022-01-20 14:54:05 +0200

    ft-header component

--- mean-frontend/src/app/app.module.ts
+++ mean-frontend/src/app/app.module.ts
@@ -1,21 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations'
+import { headerComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
-
+    PostCreateComponent,
+    headerComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
